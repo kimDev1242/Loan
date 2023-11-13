587d69f1b9dbb497b12abff60a78f5f17bf96b7c
kimdev <kimdev1242@gmail.com>
2023-11-13 15:32:19 +0900

    first commit

---
+++ src/main/java/com/practice/loan/LoanApplication.java
@@ -0,0 +1,13 @@
+package com.practice.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplication.class, args);
+    }
+
+}
